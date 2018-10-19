c65c77b8641f54d564e6bb6296ecd9b8d5ac84a7
MMMOWWW <36041208+mmmowww@users.noreply.github.com>
2018-10-19 15:53:33 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,94 +1,86 @@
 <?php
-class BaySate{
-	public $idSession; // id сесси
-	public $idItem;  // Айди товара
-	public $Qvantity; // Их количество
-	public function GG(){
-		echo "test";
-	}
-	
-}
-class BayLook extends BaySate {
-	public $tame;      // = int time(void); // Выесняем время закза для подготовки корзины
-	public $summ; // Узнаём суму
-	public $approval = bool; // Выесням одобрил ли посетитель заказ
-	public function GG(){
-		echo "test";
-	}
-}
-
-class BayStatus extends BaySate {
-	
-	public $status;
-	public function GG(){
-		echo "test";
-	}
-}
-class BayOk extends BayStatus {
-	public function GG(){
-		echo "test";
-	}
-	public $ok;
-}
-
-$UserSession = new BaYSate();
-$UserSession -> idSession = $idSession;  
-$UserSession -> idItem = $idItem; 
-$UserSession -> Qvantity = $Qvantity;
-if(!isset($UserSession -> idSession) AND !isset($UserSession -> idItem) AND !isset($UserSession -> Qvantity)) // проверяем заполнены ли они
-{
-	$userLook = new BayLook;
-	$userLook -> tame = time(); // закидываем туда время организацыии (тот момент когда посетитель посматрел и одобрил заказ)
-	$userLook -> summ = $summ; // выводим сумму
-	$userLook -> approval = "true"; // Результат того что он посматрел и одобрил (Ввели его заказ в бд для организацыии продукцыии
-
-if(!isset($userLook->tame) AND !isset($userLook->summ) AND !isset($userLook->approval)){ // если все поля заняты то содаём новый обьект
-	$Status = new BayStatus; 
-	$Status ->status = $status; // выесняем где он находиться, на почте, в пути или вообще потерян
-	// Если заказ уже можно заберать с почты или пункта выдачи вводим "OK"
-if($Status -> status == "OK"){
-	$BayOkUser = new BayOk;
-	$BayOkUser -> ok = $ok; // Пишем что поситетль забрал свой заказ и оплата прошла
-	
-}
-}
-}
-/// Задание 5
-
-
-
-/*
-class A {
-    public function foo() {
-        static $x = 0;
-        echo ++$x;
-    }
-}
-$a1 = new A();
-$a2 = new A();
-$a1->foo();
-$a2->foo();
-$a1->foo();
-$a2->foo();
-*/
-// Перемееная как бы находиться по области ввидимсти во внешней среде,
-// И при каждом вызове обьекта мы ее увеличиваему
-echo "</br>";
-class A {
-    public function foo() {
-        static $x = 0;
-        echo ++$x;
-    }
-}
-class B extends A {
-}
-$a1 = new A();
-$b1 = new B();
-$a1->foo(); //1
-$b1->foo();  //1
-$a1->foo();  //2
-$b1->foo(); //2
-
-//Видо изменение числа происходит тоько при вызове прототипа класса а
-// в бе как таковых изменений нет
 ?>
+<head>
+</head>
+<body>
+<?php
+try{
+$userMySql = 'root';
+$passMySql = '';
+$MyPdo = new PDO('mysql:host=localhost;dbname=TEST', $userMySql, $passMySql);
+}catch(PDOExeception $error){
+	echo "NAIN!";
+	echo $error->getMessage();
+	exit;
+}
+
+$sql = "SELECT * FROM `TEST`";
+$answerMySql  = $MyPdo->query($sql); 
+var_dump($answerMySql); 
+echo "</br>";
+
+
+
+
+
+
+while($row = $answerMySql->fetch()){
+
+print_r($row);
+echo "</br>";
+}
+
+class Singelton {
+public function __construct() {
+	global $DB;	
+	$this->$DB = $DB;
+	}
+public function MySqli(){
+$user = 'root';
+$pass = '';
+$MyPdo = new PDO('mysql:host=localhost;dbname=TEST', $user, $pass);
+$result = $MyPdo->query($sql);
+return $result->fetchAll(PDO::FETCH_ASSOC);	
+}
+}
+
+class Product {
+	public $name;
+	public $TwoName;
+	public function __construct(){
+		$this->name = $name;
+		$this->TwoName = $TwoName;
+	}
+	public function insert(){
+		echo "Peredal";
+		$naimus = $this->name;
+		$TwoNaimus = $this->TwoName;
+		$insertPDO = new PDO('mysql:host=localhost;dbname=TEST', "root", "");
+	    $sql = "INSERT INTO `TEST` (`id`, `Name`) VALUES (NULL, '".$naimus."')";
+	}
+	public function del(){
+		  
+		  $delPdo = new PDO('mysql:host=localhost;dbname=TEST', "root", "");
+		  $sqlEcho = "SELECT * FROM `TEST` WHERE `Name` LIKE '".$this->name."'";
+		 $sql = "DELETE FROM `TEST` WHERE `TEST`.`name` = ".$this-name."";
+		  
+		  $delPdo->query($sql);
+		  
+		 
+	}
+	public function __destruct(){
+		echo "<dev>";
+		echo "DEL";
+		echo "</dev>";
+		
+	}
+}
+
+
+$PRodyct = new Product;
+$PRodyct->name = "NAME";
+$PRodyct->TwoName = "ANAME";
+
+
+?>
+</body>
